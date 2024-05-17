cab851741e18c37922e7d297325725d94dddfde0
nurayucuncu <168285409+nurayucuncu@users.noreply.github.com>
2024-05-17 20:02:39 +0300

    C codes

---
+++ C-programming/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+
+      int r;
+      float pi=3.14159,alan;
+      printf("dairenin yaricapini giriniz:\n");
+      scanf("%d",&r);
+      alan=r*r*pi;
+      printf("dairenin alani: %f", alan );
+      
+	
+	return 0;
+}
